6df271a4916a44701566564e76aa2a90f8ef7c38
azharcse14 <mdazharcse14@gmail.com>
2021-09-15 09:14:51 +0600

    Update VarArgFunction.kt

--- src/Function/User defined functions/VarArgInFunction.kt
+++ src/Function/User defined functions/VarArgInFunction.kt
@@ -1,5 +1,16 @@
 package Kotlin.Function.`User defined functions`
 
 fun main() {
-    
+    var sum = getSum(1,2,3,4,5,6,7,8,9,0)
+    println(sum)
+}
+
+fun getSum(vararg numbers:Int):Int{
+
+    var sum = 0
+
+    for (number in numbers){
+        sum = sum + number
+    }
+    return sum
 }
