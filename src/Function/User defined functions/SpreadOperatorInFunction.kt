ef56e7bcdfee628caa071a0d8603b8b02ca9b1ba
azharcse14 <mdazharcse14@gmail.com>
2021-09-15 09:25:16 +0600

    update

--- src/Function/User defined functions/SpreadOperatorInFunction.kt
+++ src/Function/User defined functions/SpreadOperatorInFunction.kt
@@ -1,5 +1,22 @@
 package Kotlin.Function.`User defined functions`
 
+
 fun main() {
+    var array = intArrayOf(1,2,3,4,5,6,7,8,9,0)
     
+    var sum = getSumSO(*array)
+    println(sum)
+
+    var sum2 = getSumSO(1,2,3,4,5,6,7,8,9,0,*array)
+    println(sum2)
+}
+
+fun getSumSO(vararg numbers:Int):Int{
+
+    var sum = 0
+
+    for (number in numbers){
+        sum = sum + number
+    }
+    return sum
 }
