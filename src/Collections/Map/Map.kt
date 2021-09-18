f0176e728980e1c0fc5f84246e9603e4afdf20d5
azharcse14 <mdazharcse14@gmail.com>
2021-09-18 15:09:06 +0600

    update

--- src/Collections/Map/Map.kt
+++ src/Collections/Map/Map.kt
@@ -1,7 +1,7 @@
 package Kotlin.Map
 
 fun main() {
-    var myMap = mapOf(1 to "Hasina", 2 to "Khala", 44 to "jala")
+    val myMap = mapOf(1 to "Hasina", 2 to "Khala", 44 to "jala")
 
     for (m in myMap){
         println(m)
