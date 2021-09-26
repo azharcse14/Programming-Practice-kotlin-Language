c72345d2ab8e574464b5c3c2023c0092e9879e4e
azharcse14 <mdazharcse14@gmail.com>
2021-09-26 23:39:02 +0600

    update

---
+++ src/Operators/LogicalOperators.kt
@@ -0,0 +1,14 @@
+package Kotlin.Operators
+
+fun main() {
+    var x = 100
+    var y = 25
+    var z = 10
+    var result = false
+    if(x > y && x > z)
+        println(x)
+    if(x < y || x > z)
+        println(y)
+    if( result.not())
+        println("Logical operators")
+}
