67d4bfa878587ef818f96005d017d253f1dcf668
azharcse14 <mdazharcse14@gmail.com>
2021-09-26 23:29:54 +0600

    Update

---
+++ src/Operators/ArithmeticOperators.kt
@@ -0,0 +1,12 @@
+package Kotlin.Operators
+
+fun main(args: Array<String>)
+{
+    var a = 20
+    var b = 4
+    println("a + b = " + (a.plus(b)))
+    println("a - b = " + (a.minus(b)))
+    println("a * b = " + (a.times(b)))
+    println("a / b = " + (a.div(b)))
+    println("a % b = " + (a.rem(b)))
+}
