96802a4dd7688ba10e61d929d61359ca229bcba5
azharcse14 <mdazharcse14@gmail.com>
2021-09-26 23:36:55 +0600

    Update

---
+++ src/Operators/UnaryOperators.kt
@@ -0,0 +1,9 @@
+package Kotlin.Operators
+
+fun main() {
+    var e=10
+    println("First print then increment: "+ e++)
+    println("First increment then print: "+ ++e)
+    println("First print then decrement: "+ e--)
+    println("First decrement then print: "+ --e)
+}
