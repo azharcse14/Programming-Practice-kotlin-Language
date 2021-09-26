20b9fb241eea2f3751d01370e14dd05af0d44a19
azharcse14 <mdazharcse14@gmail.com>
2021-09-26 23:32:44 +0600

    Update

---
+++ src/Operators/RelationalOperators.kt
@@ -0,0 +1,13 @@
+package Kotlin.Operators
+
+fun main(args: Array<String>)
+{
+    var c = 30
+    var d = 40
+    println("c > d = "+(c>d))
+    println("c < d = "+(c.compareTo(d) < 0))
+    println("c >= d = "+(c>=d))
+    println("c <= d = "+(c.compareTo(d) <= 0))
+    println("c == d = "+(c==d))
+    println("c != d = "+(!(c?.equals(d) ?: (d === null))))
+}
