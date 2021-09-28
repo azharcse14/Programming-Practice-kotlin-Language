cefea5dfcf782a6c5c5a61f16c5565b12c14d34a
azharcse14 <mdazharcse14@gmail.com>
2021-09-28 11:54:13 +0600

    update

---
+++ src/Java/Basic/Namta.java
@@ -0,0 +1,12 @@
+package Java.Basic;
+
+public class Namta {
+    public static void main(String[] args) {
+
+        int j = 1;
+
+        for (int i=1; i<=10; i++){
+            System.out.println(j+"*"+i+"="+j*i);
+        }
+    }
+}
